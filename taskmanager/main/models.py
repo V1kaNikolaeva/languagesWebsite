b473c5ee64002bb26381133e201b3d05db570265
V1kaNikolaeva <114816050+V1kaNikolaeva@users.noreply.github.com>
2023-02-01 23:49:59 +0300

    add file

--- taskmanager/main/models.py
+++ taskmanager/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Card(models.Model):
+    word = models.CharField('Слово', max_length=20)
+    description = models.TextField('Перевод')
+
+    def __str__(self):
+        return self.word
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
-
+from .models import Card
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(reguest):
+
     return render(reguest, 'main/index.html')
 
 def tests(reguest):
     return render(reguest, 'main/tests.html')
+
+def chCards(reguest):
+    cards = Card.objects.all()
+    return render(reguest, 'main/chCards.html', {'cards': cards})
 
 
 
