fedf161b94c39e74c4befe04a18c26a058cbe386
V1kaNikolaeva <114816050+V1kaNikolaeva@users.noreply.github.com>
2023-05-14 00:59:54 +0300

    add file

--- taskmanager/main/static/main/js/searchCards.js
+++ taskmanager/main/static/main/js/searchCards.js
@@ -1,11 +1,11 @@
 'use strict'
+
 document.querySelector('.search').oninput = function() {
     const card = document.querySelectorAll('.card');
     let val = this.value.trim();
     if (val != '') {
         card.forEach(function(elem) {
             if (elem.innerText.search(val) == -1) {
-                elem.classList.add('hidden');
                 elem.classList.add('hidden');
             } else {
                 elem.classList.remove('hidden');
