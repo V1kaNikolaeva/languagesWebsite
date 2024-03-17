17f6985a70cf4f21bc5542671c28b94537ede91a
V1kaNikolaeva <114816050+V1kaNikolaeva@users.noreply.github.com>
2024-03-18 01:04:14 +0300

    adaptive

--- taskmanager/main/static/main/js/hamburger.js
+++ taskmanager/main/static/main/js/hamburger.js
@@ -6,8 +6,37 @@ hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('active')
 })
 
+function removeActive() {
+    hamburger.classList.remove('active')
+    navMenu.classList.remove('active')
+}
+
 document.querySelectorAll('nav__link').forEach(item => item.
     addEventListener('click', () => {
-        hamburger.classList.remove('active')
-        navMenu.classList.remove('active')
+        removeActive()
 }))
+
+document.addEventListener('pointerdown', (e) => {
+    const withinBoundaries = e.composedPath().includes(hamburger) || e.composedPath().includes(navMenu);
+	if (!withinBoundaries) {
+        removeActive()
+	}
+})
+
+document.addEventListener('mousedown', (e) => {
+    const withinBoundaries = e.composedPath().includes(hamburger) || e.composedPath().includes(navMenu);
+	if (!withinBoundaries) {
+        removeActive()
+	}
+})
+
+document.addEventListener('scroll', (e) => {
+    const withinBoundaries = e.composedPath().includes(hamburger) || e.composedPath().includes(navMenu);
+	if (!withinBoundaries) {
+        removeActive()
+	}
+})
+
+//Главная будет разделом учиться, где будут представлены А1 и ХСК1, со словами и пояснениями как ими пользоваться
+//Раздел аккаунт отвечает за отоброжение слов
+//Тесты тоже будут по разделам, так же можно создать один общий test.js и два массива, просто подключая нужный к html шаблону
